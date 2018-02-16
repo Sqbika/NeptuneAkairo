2b0bd35869ba488c9449a99703733dbab33717d5
Sqbika <sqbika@gmail.com>
2018-02-16 13:46:02 +0100

    Guild Setup and Listener

---
+++ Commands/Database/setupguild.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord-akairo');
+
+module.exports = class SetupCommand extends Command {
+	constructor() {
+		super('setup', {
+			description: 'Sets up the bot for the guild.',
+            userPermission: ['MANAGE_GUILD']
+		});
+	}
+
+	exec(msg) {
+        if (msg.client.settings.get(msg.guild.id, "settings", "notFound") == "notFound") {
+            msg.client.settings.set(msg.guild.id, "settings", {
+                    admins: [msg.author.id, msg.guild.ownerID],
+                    argDelete: true,
+                    leaveMeAlone: true
+            });
+            msg.client.settings.set(msg.guild.id, "args", {});
+            msg.client.settings.set(msg.guild.id, "notify", []);
+        }
+	}
+};
