1409bb297a527a0b02c30842080e32ea8688cd4b
Sqbika <sqbika@gmail.com>
2018-02-18 11:10:22 +0100

    Help updated

--- Commands/Utilities/help.js
+++ Commands/Utilities/help.js
@@ -6,32 +6,36 @@ module.exports = class HelpCommand extends Command {
 			aliases: ['help'],
 			usage: 'help <command>',
 			description: 'Lists all the commands',
-			args: [
-				{
-					id: 'command',
-					default: 'showall'
-				}
-			]
+			args: [{
+				id: 'command',
+				default: 'showall'
+			}]
 		});
 	}
 
 	exec(msg, { command }) {
 		if(command == 'showall') {
 			var result = msg.client.util.embed()
-            .setTitle(`Avaliable commands in ${msg.guild ? msg.guild.name : msg.author.username}`)
-            .setColor(msg.client.config.color)
-            .setTimestamp(new Date());
+				.setTitle(`Avaliable commands in ${msg.guild ? msg.guild.name : msg.author.username}`)
+				.setColor(msg.client.config.color)
+				.setTimestamp(new Date());
 			msg.client.commandHandler.categories.map(me => me).forEach((cat) => {
-				result.addField(cat.id, `\`\`\`\n${cat.filter(me => { if(typeof me.userPermissions === 'function') { return me.userPermissions(msg); } else { return true; } }).map(com => `${com.aliases[0]} | ${com.description}`).join('\n')}\n\`\`\``);
+				result.addField(cat.id, `\`\`\`\n${cat.filter(me => hasPermission(msg, me)).map(com => `
+					${
+						com.aliases[0]
+					} | ${
+						com.description
+					}
+					`).join('\n')}\n\`\`\``);
 			});
 			msg.channel.send({ embed: result });
 		} else {
 			var cmd = this.client.commandHandler.resolver.type('commandAlias')(command);
 			if(cmd) {
 				var result = msg.client.util.embed()
-                .setColor(msg.client.config.color)
-                .setTimestamp(new Date())
-				.addField(`Avaliable information about ${cmd.id}`, funcDesc(cmd));
+					.setColor(msg.client.config.color)
+					.setTimestamp(new Date())
+					.addField(`Avaliable information about ${cmd.id}`, funcDesc(cmd));
 				msg.channel.send({ embed: result });
 			} else {
 				msg.reply('Command not found.');
@@ -60,4 +64,22 @@ ${command.args.map(ar => `  -**Name**: ${ar.id}
  **User Permission Required**: ${command.userPermissions ? `Yes: ${command.userPermissions}` : 'No'}
  **Bot Permission Required**: ${command.clientPermissions ? `Yes: ${command.clientPermissions}` : 'No'}
 `;
+}
+
+function hasPermission(msg, command) {
+	var result = true;
+	if(typeof command.userPermissions === 'function') {
+		result = command.userPermissions(msg);
+	}
+	if(command.ownerOnly) {
+		return msg.author.id == msg.client.config.ownerID;
+	}
+	if(msg.guild) {
+		if(command.clientPermissions) {
+			result = msg.guild.me.hasPermission(command.clientPermissions);
+		}
+	} else {
+		result = command.channelRestriction !== 'guild';
+	}
+	return result;
 }
