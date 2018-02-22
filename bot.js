d0811f1462f204d8aa339bc9a66ef1a2f60b2b2c
Sqbika <sqbika@gmail.com>
2018-02-22 19:58:11 +0100

    unhandledRejection

--- bot.js
+++ bot.js
@@ -5,3 +5,7 @@ const config = require('./config.json');
 const client = new NeptuneClient(config);
 
 client.start(require(config.loginToken).loginToken);
+
+process.on('unhandledRejection', error => {
+    console.log('unhandledRejection', error.message);
+  });
