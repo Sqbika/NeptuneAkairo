61ab606ec604284869a2ed4a5b254c84000e602c
Sqbika <sqbika@gmail.com>
2018-04-01 16:40:54 +0200

    Fixed Another Small error

--- Commands/Database/addmods.js
+++ Commands/Database/addmods.js
@@ -24,11 +24,11 @@ module.exports = class AddModsCommand extends Command {
 		return msg.client.settings.get(msg.guild.id, 'settings', []).admins.indexOf(msg.author.id) !== -1;
 	}
 
-	exec(msg, { user }) {
+	exec(msg, { role }) {
 		if(msg.deletable && msg.client.settings.get(msg.guild.id, 'settings').argDelete) msg.delete();
         var argObject = msg.client.settings.get(msg.guild.id, 'settings');
         if (!argObject.mods) argObject.mods = [];
-		argObject.mods.push(user.id);
+		argObject.mods.push(role.id);
 		msg.client.settings.set(msg.guild.id, 'settings', argObject);
 		msg.reply('Succesfully added role to the mods list.').then((me) => me.delete(10000));
 	}
--- Commands/Database/removemods.js
+++ Commands/Database/removemods.js
@@ -24,10 +24,10 @@ module.exports = class RemoveModsCommand extends Command {
 		return msg.client.settings.get(msg.guild.id, 'settings', []).admins.indexOf(msg.author.id) !== -1;
 	}
 
-	exec(msg, { user }) {
+	exec(msg, { role }) {
 		if(msg.deletable && msg.client.settings.get(msg.guild.id, 'settings').argDelete) msg.delete();
 		var argObject = msg.client.settings.get(msg.guild.id, 'settings');
-		argObject.mods.splice(argObject.mods.indexOf(user.id), 1);
+		argObject.mods.splice(argObject.mods.indexOf(role.id), 1);
 		msg.client.settings.set(msg.guild.id, 'settings', argObject);
 		msg.reply('Succesfully removed role from the mods list.').then((me) => me.delete(10000));
 	}
