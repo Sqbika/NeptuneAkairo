c19a31ffe0f44f97bd8bea64c83fe10af36337bf
Sqbika <sqbika@gmail.com>
2018-04-01 13:10:57 +0200

    Rot Command

---
+++ Commands/Ciphers/rot.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord-akairo');
+const abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+module.exports = class RotCommand extends Command {
+	constructor() {
+		super('rot', {
+			aliases: ['rot'],
+			usage: 'rot <number:1-27> <text:Rest>',
+            description: 'Rotates the text according to the number',
+            typing: true,
+            args: [{
+                id: 'number',
+                type: integer,
+            }, {
+                id: 'text',
+                match: 'rest'
+            }]
+        });
+	}
+
+	async exec(msg, { rot, text }) {
+        var textarr = text.split('');
+        var result = "";
+        for (var i = 0; i < text.length; i++) {
+            if (abc.indexOf(textarr[i]) !== -1) {
+                if (textarr[i].toLowerCase() !== textarr[i]) {
+                    result += abc[abc.indexOf(textarr[i].toLowerCase()) + number].toUpperCase();
+                } else {
+                    result += abc[abc.indexOf(textarr[i]) + number];
+                }
+            } else {
+                result += textarr[i];
+            }
+        }
+        msg.reply("Rotated Text with `" + number + "`: **" + result + "**");
+	}
+};
