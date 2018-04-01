d639bc8f41511e4c9785dc5a6717182c9e856e5b
Sqbika <sqbika@gmail.com>
2018-04-01 14:37:01 +0200

    .add instead of .load

--- Commands/Admin/reinit.js
+++ Commands/Admin/reinit.js
@@ -31,7 +31,7 @@ module.exports = class ReinitCommand extends Command {
             TEMPCategory.removeAll();
             var i = 0;
             fs.readdirSync(path.join(catpath, '..')).forEach((file) => {
-                msg.client.commandHandler.load(file);
+                msg.client.commandHandler.add(file);
                 i++;
             });
             msg.reply(`Reloaded **${category}**. Loaded modules: **${i}**`);
