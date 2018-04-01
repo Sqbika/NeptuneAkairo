5a3fa5f666daeeb364738dd00686b7a0a0b48fc2
Sqbika <sqbika@gmail.com>
2018-04-01 14:39:52 +0200

    Filepath fixed

--- Commands/Admin/reinit.js
+++ Commands/Admin/reinit.js
@@ -31,8 +31,7 @@ module.exports = class ReinitCommand extends Command {
             TEMPCategory.removeAll();
             var i = 0;
             fs.readdirSync(path.join(catpath, '..')).forEach((file) => {
-                console.log(file);
-                msg.client.commandHandler.add(file);
+                msg.client.commandHandler.add(path.join(catpath, '..', file));
                 i++;
             });
             msg.reply(`Reloaded **${category}**. Loaded modules: **${i}**`);
