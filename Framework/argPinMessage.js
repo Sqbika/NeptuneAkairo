3cf9fe248c275460155cc63991c0285e6a9c3bfe
Sqbika <sqbika@github.com>
2018-04-04 22:29:29 +0200

    Added PINMessages Embed Stuff Upload and Magic

--- Framework/argPinMessage.js
+++ Framework/argPinMessage.js
@@ -1,4 +1,8 @@
-const { createCanvas, loadImage } = require('canvas');
+const {
+    createCanvas,
+    loadImage
+} = require('canvas');
+const fs = require('jetpack');
 
 let messages = [];
 const width = 350;
@@ -14,7 +18,7 @@ async function fetchMessage(pinMessage) {
     console.log(channel + " / " + pinMessage.channel);
     var msg;
     try {
-    msg  = await channel.fetchMessage(pinMessage.msgID);
+        msg = await channel.fetchMessage(pinMessage.msgID);
     } catch (e) {
         msg = undefined;
     }
@@ -28,13 +32,17 @@ function addMessage(pinMessage) {
 }
 
 async function updateMessage(pinMessage) {
-    var image = drawImage(pinMessage);
+    drawImage(pinMessage);
     var msg = await fetchMessage(pinMessage);
 
-    if (msg == undefined) 
-        msg = client.channels.get(pinMessage.channel).send({embed: client.util.embed().setImage(image.toBuffer())});
-    else 
-        msg.edit({embed: client.util.embed().setImage(image.toBuffer())});
+    if (msg == undefined)
+        msg = client.channels.get(pinMessage.channel).send({
+            embed: client.util.embed().setImage({url: `https://sqbika.win/atlas/pinmessages/${pinMessage.arg}.png?${Math.random()}`})
+        });
+    else
+        msg.edit({
+            embed: client.util.embed().setImage({url: `https://sqbika.win/atlas/pinmessages/${pinMessage.arg}.png?${Math.random()}`})
+        });
 
     var guildID = client.channels.get(pinMessage.channel).guild.id;
     var a = client.settings.get(guildID, 'args')
@@ -51,9 +59,13 @@ function periodicUpdate() {
     if (periodicUpdateLimitter >= 12) {
         periodicUpdateLimitter = 0;
         messages.forEach(pinMessage => {
-            var image = drawImage(pinMessage);
+            drawImage(pinMessage);
             var message = fetchMessage(pinMessage).then(message => {
-                message.edit({embed: client.util.embed().setImage(image.toBuffer())});
+                message.edit({
+                    embed: client.util.embed().setImage({
+                        url: `https://sqbika.win/atlas/pinmessages/${pinMessage.arg}.png?${Math.random()}`
+                    })
+                });
             });
         })
     }
@@ -64,7 +76,7 @@ function loadMessages() {
         guild.args.forEach(args => {
             if (typeof args.pinMessage !== "undefined") {
                 messages.push(args.pinMessage);
-                if (args.pinMessage.id > maxID) maxID = args.pinMessage.id +1;
+                if (args.pinMessage.id > maxID) maxID = args.pinMessage.id + 1;
             }
         })
     });
@@ -76,41 +88,41 @@ function drawImage(pinMessage) {
     const canvas = createCanvas(350, 300);
     var ctx = canvas.getContext("2d");
 
-    ctx.fillStyle="#36393e";
-    ctx.fillRect(0,0,500,300);
-    
-    ctx.fillStyle="#ffffff";
-    
+    ctx.fillStyle = "#36393e";
+    ctx.fillRect(0, 0, 500, 300);
+
+    ctx.fillStyle = "#ffffff";
+
     ctx.font = "13.25px Arial";
     ctx.fillText("Update:" + new Date().toString(), ctx.measureText(pinMessage.arg).width + 8, ctx.measureText("M").width);
-    ctx.fillText(pinMessage.arg,1,ctx.measureText("M").width);
-    
-    ctx.strokeStyle="#ccffcc";
+    ctx.fillText(pinMessage.arg, 1, ctx.measureText("M").width);
+
+    ctx.strokeStyle = "#ccffcc";
     ctx.fillStyle = "#ff0077";
     ctx.strokeRect(-1, -1, ctx.measureText(pinMessage.arg).width + 5, ctx.measureText("M").width + 5);
-    ctx.strokeRect(ctx.measureText(pinMessage.arg).width + 4, -1, 500, ctx.measureText("M").width + 5); 
-    
+    ctx.strokeRect(ctx.measureText(pinMessage.arg).width + 4, -1, 500, ctx.measureText("M").width + 5);
+
     var lineHeight = (300 - ctx.measureText("M").width + 5) / 2;
-    
+
     ctx.beginPath();
     ctx.moveTo(0, (300 - ctx.measureText("M").width + 5) / 2);
     ctx.lineTo(500, (300 - ctx.measureText("M").width + 5) / 2);
     ctx.stroke();
-    
+
     ctx.font = "20px Arial";
-    var whatHappenedStartin = ctx.measureText("M").width*2;
-    
-    
+    var whatHappenedStartin = ctx.measureText("M").width * 2;
+
+
     ctx.fillText("What Happened:", 1, whatHappenedStartin);
     ctx.fillStyle = "#cccccc";
     ctx.font = "15px Arial";
     var text = pinMessage.whatHappened.text;
     var textLength = ctx.measureText(text).width;
-    var done = false; 
-    var a = Math.floor(textLength/width);
-    a > 5? a = 5 : a = a;
+    var done = false;
+    var a = Math.floor(textLength / width);
+    a > 5 ? a = 5 : a = a;
     var letter = ctx.measureText("M").width;
-    for (var i = 1; i < a+1; i++) {
+    for (var i = 1; i < a + 1; i++) {
         var ehh = true;
         var b = 30;
         var fill;
@@ -124,14 +136,14 @@ function drawImage(pinMessage) {
             } else {
                 ehh = true;
             }
-        } while(ehh);
+        } while (ehh);
         var text = text.slice(b, text.length);
-        ctx.fillText(fill, 1, whatHappenedStartin + (20*i));
+        ctx.fillText(fill, 1, whatHappenedStartin + (20 * i));
     }
-    
+
     ctx.fillStyle = "#22cc55";
     ctx.fillText("When:" + pinMessage.whatHappened.date, 1, lineHeight - 5);
-    
+
     ctx.font = "20px Arial";
     var waitingStart = lineHeight + ctx.measureText("M").width + 5;
     ctx.fillStyle = "#ff0077";
@@ -141,11 +153,11 @@ function drawImage(pinMessage) {
 
     var text = pinMessage.waitingFor.text;
     var textLength = ctx.measureText(text).width;
-    var done = false; 
-    var a = Math.floor(textLength/width);
-    a > 5? a = 5 : a = a;
+    var done = false;
+    var a = Math.floor(textLength / width);
+    a > 5 ? a = 5 : a = a;
     var letter = ctx.measureText("M").width;
-    for (var i = 1; i < a+1; i++) {
+    for (var i = 1; i < a + 1; i++) {
         var ehh = true;
         var b = 30;
         var fill;
@@ -159,17 +171,26 @@ function drawImage(pinMessage) {
             } else {
                 ehh = true;
             }
-        } while(ehh);
+        } while (ehh);
         var text = text.slice(b, text.length);
-        ctx.fillText(fill, 1, waitingStart + (20*i));
-    }    
+        ctx.fillText(fill, 1, waitingStart + (20 * i));
+    }
     ctx.fillStyle = "#22cc55";
     ctx.fillText("Speculation:" + pinMessage.waitingFor.date, 1, 290);
 
-    return canvas;
+    fs.write('/var/www/html/atlas/pinmessages/' + pinMessage.arg + ".png", canvas.toBuffer());
 }
 
-module.exports = {setup, fetchMessage, updateMessage, periodicUpdate, loadMessages, drawImage, addMessage, client};
+module.exports = {
+    setup,
+    fetchMessage,
+    updateMessage,
+    periodicUpdate,
+    loadMessages,
+    drawImage,
+    addMessage,
+    client
+};
 
 /*
 {
