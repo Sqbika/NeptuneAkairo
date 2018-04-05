49f9dacb265d7f8057b5a772f3b18b20704ad097
Sqbika <sqbika@gmail.com>
2018-04-05 12:35:42 +0200

    Added waiting for date parsers

--- Commands/ARG Related/pinmessage.js
+++ Commands/ARG Related/pinmessage.js
@@ -1,6 +1,7 @@
 const {
 	Command
 } = require('discord-akairo');
+const time = require('moment');
 
 module.exports = class PinMessageCommand extends Command {
 	constructor() {
@@ -11,14 +12,14 @@ module.exports = class PinMessageCommand extends Command {
 			channelRestriction: 'guild',
 			args: [{
                 id: 'sub',
-                type: [['waitingfor', 'wf', 'waiting', 'wfor'], ['whathappened', 'wh', 'happened', 'happen']],
+                type: [['waitingfor', 'wf', 'waiting', 'wfor'], ['whathappened', 'wh', 'happened', 'happen'], ['waitingfordate', 'wfd', 'waitingfd']],
                 prompt: {
 					retries: 2,
-					start: (msg) => `<@!${msg.author.id}> Please choose which one you want to update: **waitingfor** or **whathappened**`,
-					retry: (msg) => `<@!${msg.author.id}> Please choose either **waitingfor** or **whathappened**`
+					start: (msg) => `<@!${msg.author.id}> Please choose which one you want to update: **waitingfor**, **whathappened** or **waitingfordate**`,
+					retry: (msg) => `<@!${msg.author.id}> Please choose either **waitingfor**, **whathappened** or **waitingfordate**`
 				},
-                description: 'The sub between updating waitingfor and whathappened',
-                usage: '<waitingfor,wf,waiting,wfor> or <whathappened,wh,happened,happen>'
+                description: 'The sub between updating waitingfor, whathappened and waitingfordate',
+                usage: '<waitingfor,wf,waiting,wfor>, <whathappened,wh,happened,happen> or <waitingfordate,wfd,waitingfd>'
             },{
 				id: 'arg2',
 				type: (word, msg) => Object.keys(msg.client.settings.get(msg.guild.id, 'args')).indexOf(word) !== -1 ? true : undefined,
@@ -64,25 +65,103 @@ module.exports = class PinMessageCommand extends Command {
                     date: new Date().toString()
                 }
             }
-            if (sub == "waitingfor") {
-                pinMessage.waitingFor.text = text;
-                pinMessage.waitingFor.date = new Date().toString()
-            }
-            if (sub == "whathappened") {
-                pinMessage.whatHappened.text = text;
-                pinMessage.whatHappened.date = new Date().toString()
+            switch(sub) {
+                case "waitingfor":
+                    pinMessage.waitingFor.text = text;
+                    break;
+                case "whathappened":
+                    pinMessage.whatHappened.text = text;
+                    pinMessage.whatHappened.date = new Date().toString();
+                    break;
+                case "waitingfordate":
+                    var parsedTime = moment(text);
+                    if (!parsedTime.isValid())
+                        parsedTime = moment(parseUgly(text).absolute);
+                    pinMessage.waitingFor.date = parsedTime.toDate().toString();
+                    break;
             }
             msg.client.pinMessage.addMessage(pinMessage);
         }
-        if (sub == "waitingfor") {
-            pinMessage.waitingFor.text = text;
-            pinMessage.waitingFor.date = new Date().toString()
-        }
-        if (sub == "whathappened") {
-            pinMessage.whatHappened.text = text;
-            pinMessage.whatHappened.date = new Date().toString()
+        switch(sub) {
+            case "waitingfor":
+                pinMessage.waitingFor.text = text;
+                break;
+            case "whathappened":
+                pinMessage.whatHappened.text = text;
+                pinMessage.whatHappened.date = new Date().toString();
+                break;
+            case "waitingfordate":
+                var parsedTime = moment(text);
+                if (!parsedTime.isValid())
+                    parsedTime = moment(parseUgly(text).absolute);
+                pinMessage.waitingFor.date = parsedTime.toDate().toString();
+                break;
         }
         msg.client.pinMessage.updateMessage(pinMessage);
         msg.reply("Added and updated PinMessage. Will show up in a few seconds.").then(res => res.delete(5000));
 	}
 };
+
+
+//Source: Zephy
+function parseUgly(timeout) {
+	timeout = timeout.replace(/\s+/g, '');
+	var SECONDS = /(\d+) *(?:seconds|seconds|sec|s)/i;
+	var MINUTES = /(\d+) *(?:minutes|minute|min|m)/i;
+	var HOURS = /(\d+) *(?:hours|hour|h)/i;
+	var DAYS = /(\d+) *(?:days|days|d)/i;
+	var WEEKS = /(\d+) *(?:weeks|week|w)/i;
+
+
+	var delta = 0;
+
+	var hours = 0;
+	var minutes = 0;
+	var seconds = 0;
+	var days = 0;
+	var years = 0;
+	var weeks = 0;
+
+	var ss = SECONDS.exec(timeout);
+	if(ss && ss[1]) {
+		delta += +ss[1];
+		seconds += +ss[1];
+	}
+
+	ss = MINUTES.exec(timeout);
+	if(ss && ss[1]) {
+		delta += +ss[1] * 60;
+		minutes += +ss[1];
+	}
+
+	ss = HOURS.exec(timeout);
+	if(ss && ss[1]) {
+		delta += +ss[1] * 60 * 60;
+		hours += +ss[1];
+	}
+
+	ss = DAYS.exec(timeout);
+	if(ss && ss[1]) {
+		delta += +ss[1] * 60 * 60 * 24;
+		days += +ss[1];
+	}
+
+	ss = WEEKS.exec(timeout);
+	if(ss && ss[1]) {
+		delta += +ss[1] * 60 * 60 * 24 * 7;
+		days += +ss[1];
+	}
+
+	if(isNaN(hours + minutes + seconds) || delta < 1) return false;
+	return {
+		absolute: new Date().getTime() + (delta * 1000),
+		relative: delta * 1000,
+		seconds: seconds,
+		minutes: minutes,
+		hours: hours,
+		days: days,
+		weeks: weeks,
+		years: years,
+		delta: delta
+	};
+}
